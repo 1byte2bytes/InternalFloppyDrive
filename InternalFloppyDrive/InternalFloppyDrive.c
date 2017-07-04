660ba53d1e738c91cb3011412af1a40ec08dcfa2
Sydney Erickson <sydneyerickson@icloud.com>
2017-07-04 12:27:44 -0700

    Remove duplicated code and add variable for if floppy motor is on

--- InternalFloppyDrive/InternalFloppyDrive.c
+++ InternalFloppyDrive/InternalFloppyDrive.c
@@ -10,6 +10,7 @@
 #include <mach/mach_types.h>
 
 #include "FDC.h"
+#include "IO.h"
 
 typedef int bool;
 enum { false, true };
@@ -28,27 +29,7 @@ enum { false, true };
 */
 
 bool DoFDCCheck = false;
- 
- 
-
-/* OSDev stuff */
-static inline void outb(uint16_t port, uint8_t val)
-{
-    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
-    /* There's an outb %al, $imm8  encoding, for compile-time constant port numbers that fit in 8b.  (N constraint).
-     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
-     * The  outb  %al, %dx  encoding is the only option for all other cases.
-     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
-}
-
-static inline uint8_t inb(uint16_t port)
-{
-    uint8_t ret;
-    asm volatile ( "inb %1, %0"
-                  : "=a"(ret)
-                  : "Nd"(port) );
-    return ret;
-}
+bool FloppyMotorOn = false;
 
 kern_return_t InternalFloppyDrive_start(kmod_info_t * ki, void *d);
 kern_return_t InternalFloppyDrive_stop(kmod_info_t *ki, void *d);
